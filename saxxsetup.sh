860989f3643c95f4e5de7e4b0bc9bed7f9689951
theuitown <nitingoswami473@gmail.com>
2020-05-30 23:08:12 +0530

    Support for termux_android

--- saxxsetup.sh
+++ saxxsetup.sh
@@ -1,25 +1,28 @@
 if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    sudo apt-get install --force-yes --yes figlet;
+    pkg install --force-yes --yes figlet;
+fi
+if [ $(dpkg-query -W -f='${Status}' python 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+    pkg install --force-yes --yes python;
 fi
 if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    sudo apt-get install --force-yes --yes python3;
+    pkg install --force-yes --yes python3;
 fi
 if [ $(dpkg-query -W -f='${Status}' brotli 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    sudo apt-get install --force-yes --yes brotli;
+    pkg install --force-yes --yes brotli;
 fi
 if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    sudo apt-get install --force-yes --yes zip;
-fi
-if [ $(dpkg-query -W -f='${Status}' img2simg 2>/dev/null | grep -c "ok installed") -eq 0 ];
-then
-    sudo apt-get install --force-yes --yes img2simg;
+    pkg install --force-yes --yes zip;
 fi
 if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    sudo apt-get install --force-yes --yes git;
+    pkg install --force-yes --yes git;
 fi
+rm -rf output
+apt-get update && apt-get upgrade
+
 git clone https://github.com/xpirt/img2sdat.git
