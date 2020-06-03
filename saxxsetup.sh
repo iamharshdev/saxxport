5c183ed0d966f58540bd57684ab560f8d6288ad4
theuitown <nitingoswami473@gmail.com>
2020-06-03 17:47:30 +0530

    idk

--- saxxsetup.sh
+++ saxxsetup.sh
@@ -1,25 +1,25 @@
-if [ $(dpkg-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' figlet 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes figlet;
+    apt install --force-yes --yes figlet;
 fi
-if [ $(dpkg-query -W -f='${Status}' python 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' python 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes python;
+    apt install --force-yes --yes python;
 fi
-if [ $(dpkg-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' python3 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes python3;
+    apt install --force-yes --yes python3;
 fi
-if [ $(dpkg-query -W -f='${Status}' brotli 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' brotli 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes brotli;
+    apt install --force-yes --yes brotli;
 fi
-if [ $(dpkg-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' zip 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes zip;
+    apt install --force-yes --yes zip;
 fi
-if [ $(dpkg-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
+if [ $(dapt-query -W -f='${Status}' git 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
-    pkg install --force-yes --yes git;
+    apt install --force-yes --yes git;
 fi
 apt-get update && apt-get upgrade
